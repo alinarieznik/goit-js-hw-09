e203819e880d11a85cb51d85a439c10aa9da2c6a
Alina Rieznik <alina.raicheva97@gmail.com>
2023-04-02 23:39:35 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const bodyEL = document.querySelector('body');
+// console.log(bodyEL);
+const buttonStartEl = bodyEL.querySelector('[data-start]');
+// console.log(buttonStartEl);
+const buttonStopEl = bodyEL.querySelector('[data-stop]');
+// console.log(buttonStopEl);
 
+buttonStartEl.addEventListener('click', onStartButtonClick);
+buttonStopEl.addEventListener('click', onStopButtonClick);
+
+let timerInterval = null;
+
+function onStartButtonClick(e) {
+  timerInterval = setInterval(() => {
+    bodyEL.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (e.target) {
+    buttonStartEl.setAttribute('disabled', true);
+  }
+}
+
+function onStopButtonClick(e) {
+  clearInterval(timerInterval);
+  if (e.target) {
+    buttonStartEl.removeAttribute('disabled');
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
